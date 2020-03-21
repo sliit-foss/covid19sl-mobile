d1004404b0a6b0b7ef2aef47ed7586907ea7e398
Nandun Thilina Bandara <ntbandara3@hotmail.com>
2020-03-21 17:19:44 +0530

    Overview card ui changes

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 
 class DashboardState extends State<DashboardPage> {
+  final int totalLocalCount = 76;
+  final int localDeaths = 0;
+  final int localRecovered = 1;
+  final int localInHospital = 245;
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -14,9 +18,7 @@ class DashboardState extends State<DashboardPage> {
             ),
             Row(
               crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Text('COVID-19 SL')
-              ],
+              children: <Widget>[Text('COVID-19 SL')],
             ),
             SizedBox(
               height: 30,
@@ -30,15 +32,63 @@ class DashboardState extends State<DashboardPage> {
                     padding: const EdgeInsets.only(
                         left: 16.0, right: 16.0, top: 16.0, bottom: 32.0),
                     child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.center,
                       children: <Widget>[
                         Row(
-                          children: <Widget>[
-                            Text("Hello")
-                          ],
+                          children: <Widget>[Text("Total Local Cases")],
+                        ),
+                        Row(
+                          children: <Widget>[Text(totalLocalCount.toString())],
                         ),
                         SizedBox(
-                          height: 4,
+                          height: 10,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            // Local Deaths
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: <Widget>[
+                                Row(children: <Widget>[
+                                  Text(localDeaths.toString())
+                                ],),
+                                Row(
+                                  children: <Widget>[Text("Deaths")],
+                                )
+                              ],
+                            ),
+
+                            // Local Recovered
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: <Widget>[
+                                Row(
+                                  children: <Widget>[
+                                    Text(localRecovered.toString())
+                                  ],
+                                ),
+                                Row(
+                                  children: <Widget>[Text("Recovered")],
+                                )
+                              ],
+                            ),
+
+                            // Local total in hospital
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: <Widget>[
+                                Row(
+                                  children: <Widget>[
+                                    Text(localInHospital.toString())
+                                  ],
+                                ),
+                                Row(
+                                  children: <Widget>[Text("In Hospitals")],
+                                )
+                              ],
+                            )
+                          ],
                         )
                       ],
                     ),
